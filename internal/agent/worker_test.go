b9c3c6d4653db15da7ab8a6340e083e4b9d3605d
Sofiya <sofiytula71@gmail.com>
2025-05-05 21:29:17 +0300

    worker test

--- internal/agent/worker_test.go
+++ internal/agent/worker_test.go
@@ -1,53 +1,115 @@
 package agent
 
-// import (
-// 	"context"
-// 	"net"
-// 	"testing"
-// 	"time"
+import (
+	"context"
+	"log"
+	"net"
+	"os"
+	"testing"
+	"time"
 
-// 	"github.com/bulbosaur/calculator-with-authorization/proto"
-// 	"github.com/stretchr/testify/assert"
-// 	"google.golang.org/grpc"
-// 	"google.golang.org/grpc/credentials/insecure"
-// )
+	"github.com/bulbosaur/calculator-with-authorization/proto"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
 
-// func TestWorkerIntegration(t *testing.T) {
-// 	srv, testServer := StartTestServer()
-// 	defer srv.Stop()
+func TestWorker(t *testing.T) {
+	srv, testServer := startTestServer()
+	defer srv.Stop()
 
-// 	testServer.tasks[1] = &proto.Task{
-// 		Id:        1,
-// 		Arg1:      15,
-// 		Arg2:      25,
-// 		Operation: "+",
-// 	}
+	testServer.tasks[1] = &proto.Task{
+		Id:           1,
+		ExpressionId: 1,
+		Arg1:         15,
+		Arg2:         5,
+		Operation:    "-",
+	}
 
-// 	conn, err := grpc.NewClient(
-// 		"bufnet",
-// 		context.Background(),
-// 		grpc.WithContextDialer(func(ctx context.Context, s string) (net.Conn, error) {
-// 			return lis.Dial()
-// 		}),
-// 		grpc.WithTransportCredentials(insecure.NewCredentials()),
-// 	)
-// 	assert.NoError(t, err)
+	conn, err := grpc.NewClient(
+		"passthrough:///bufnet",
+		grpc.WithContextDialer(func(ctx context.Context, _ string) (net.Conn, error) {
+			return lis.DialContext(ctx)
+		}),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock(),
+	)
+	if err != nil {
+		t.Fatalf("Connection error: %v", err)
+	}
+	defer conn.Close()
 
-// 	agent := &GRPCAgent{
-// 		client: proto.NewTaskServiceClient(conn),
-// 		conn:   conn,
-// 	}
-// 	Workers = 1
+	agent := &GRPCAgent{
+		client: proto.NewTaskServiceClient(conn),
+		conn:   conn,
+	}
 
-// 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
-// 	defer cancel()
+	Workers = 1
+	go agent.worker(1)
 
-// 	go agent.worker(1)
+	select {
+	case req := <-testServer.received:
+		assert.Equal(t, float64(10), req.Result, "Incorrect result")
+		assert.Equal(t, int32(1), req.TaskId, "incorrect task ID")
+	case <-time.After(3 * time.Second):
+		t.Fatal("Timeout waiting for result")
+	}
+}
 
-// 	select {
-// 	case req := <-testServer.received:
-// 		assert.Equal(t, float64(40), req.Result)
-// 	case <-ctx.Done():
-// 		t.Fatal("Timeout waiting for worker processing")
-// 	}
-// }
+func TestWorkerErrorHandling(t *testing.T) {
+	srv, testServer := startTestServer()
+	defer srv.Stop()
+
+	testServer.tasks[1] = &proto.Task{
+		Id:        1,
+		Arg1:      0,
+		Arg2:      0,
+		Operation: "invalid_operation",
+	}
+
+	conn, err := grpc.NewClient(
+		"passthrough:///bufnet",
+		grpc.WithContextDialer(func(ctx context.Context, _ string) (net.Conn, error) {
+			return lis.DialContext(ctx)
+		}),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock(),
+	)
+	if err != nil {
+		t.Fatalf("Connection error: %v", err)
+	}
+	defer conn.Close()
+
+	agent := &GRPCAgent{
+		client: proto.NewTaskServiceClient(conn),
+		conn:   conn,
+	}
+
+	logChan := make(chan string, 2)
+	log.SetOutput(&logWriter{logs: logChan})
+	defer log.SetOutput(os.Stderr)
+
+	Workers = 1
+	go agent.worker(1)
+
+	var logMsg string
+	for i := 0; i < 2; i++ {
+		select {
+		case msg := <-logChan:
+			logMsg += msg
+		case <-time.After(3 * time.Second):
+			t.Fatal("Timeout waiting for log message")
+		}
+	}
+
+	assert.Contains(t, logMsg, "execution error task ID-1", "No execution error log message")
+}
+
+type logWriter struct {
+	logs chan string
+}
+
+func (w *logWriter) Write(p []byte) (n int, err error) {
+	w.logs <- string(p)
+	return len(p), nil
+}
