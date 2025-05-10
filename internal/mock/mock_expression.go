b22aa0dd2c0023b04558c44a58f36d84632079ac
Sofiya <sofiytula71@gmail.com>
2025-05-10 18:10:07 +0300

    fix code style

--- internal/mock/mock_expression.go
+++ internal/mock/mock_expression.go
@@ -4,31 +4,31 @@ import (
 	"github.com/bulbosaur/calculator-with-authorization/internal/models"
 )
 
-// MockExpressionModel — минимальная реализация ExpressionModel для тестов
-type MockExpressionModel struct {
+// ExpressionModel — минимальная реализация ExpressionModel для тестов
+type ExpressionModel struct {
 	models.ExpressionRepository
 }
 
 // GetTask — заглушка, возвращающая nil и ошибку
-func (m *MockExpressionModel) GetTask() (*models.Task, int, error) {
+func (m *ExpressionModel) GetTask() (*models.Task, int, error) {
 	return nil, 0, nil
 }
 
 // UpdateTaskResult — заглушка
-func (m *MockExpressionModel) UpdateTaskResult(id int, result float64, err string) error {
+func (m *ExpressionModel) UpdateTaskResult(id int, result float64, err string) error {
 	return nil
 }
 
 // GetExpression — заглушка
-func (m *MockExpressionModel) GetExpression(id int) (*models.Expression, error) {
+func (m *ExpressionModel) GetExpression(id int) (*models.Expression, error) {
 	return &models.Expression{}, nil
 }
 
 // Insert — заглушка
-func (m *MockExpressionModel) Insert(expr string, userID int) (int, error) {
+func (m *ExpressionModel) Insert(expr string, userID int) (int, error) {
 	return 1, nil
 }
 
 // UpdateStatus — заглушка
-func (m *MockExpressionModel) UpdateStatus(id int, status string) {
+func (m *ExpressionModel) UpdateStatus(id int, status string) {
 }
